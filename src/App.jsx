9b55573c5f00c9bbebecd97c473d7c6ff9c26a96
Siddartha Datta <sid2002official@gmail.com>
2024-12-29 15:01:26 +0530

    Added a new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // import React from "react"
-import Navbar from "./components/Navbar.jsx"
-import Home from './pages/Home.jsx'
-// import LetsTalk from "./pages/LetsTalk"
+// import Navbar from "./components/Navbar.jsx"
+// import Home from './pages/Home.jsx'
+import LetsTalk from "./pages/LetsTalk"
 
 function App() {
   
 
   return (
     <div className="bg-[#ecf0f1]">
-      <Navbar />
-      <Home /> 
-      {/* <LetsTalk/> */}
+      {/* <Navbar />
+      <Home />  */}
+      <LetsTalk/>
     </div>
   )
 }
